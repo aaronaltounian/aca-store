f5ad9dc8802505920c940ba2d22d0bf13faaeb0a
Aaron Altounian <aaron.altounian@gmail.com>
2019-04-16 21:03:39 -0500

    added dropdown box for QTY in product focus

--- public/focus-product.js
+++ public/focus-product.js
@@ -10,10 +10,18 @@ const focusProduct = (index) => {
         imgUrl,
         } = products[index];
 
+    let options;
+    function selectOptions() {
+        for(let i = 1; i <= 10; i++) {
+            options += `<option value=${i}>${i}</value>`
+        }
+    }
+    selectOptions();
     // use deconstructed properties to populate some HTML:
     item.innerHTML =   `<img src=${imgUrl} />
                         <h1>${name}</h1>
                         <h2>${price}</h2>
+                        <select>${options}</select>
                         <button onclick='addToCart(${index})'>Add To Cart</button>
                         <p>${description}</p>`;
 }
